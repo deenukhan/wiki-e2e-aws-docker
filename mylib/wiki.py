2cc31f2997d59a140361412e5415777cb1650dbd
Deenu Khan <dr.deenukhan001@gmail.com>
2023-07-24 09:04:07 +0000

    Added wiki.py logic

--- mylib/wiki.py
+++ mylib/wiki.py
@@ -1,1 +1,30 @@
-from wiki import Wiki
+import wikipedia as wiki
+
+#build a function to get the summary of a wikipedia page
+def get_summary(topic):
+    """
+    This function returns the summary of a wikipedia page
+    :param topic: the topic of the wikipedia page
+    """
+    return wiki.summary(topic)
+
+#build a function to search the wikipedia page for a match
+def search_wiki(topic):
+    """
+    This function searches the wikipedia page for a match
+    :param topic: the topic of the wikipedia page
+    """
+    return wiki.search(topic)
+
+#build a function to return the wikipedia page
+def get_page(topic):
+    """
+    This function returns the wikipedia page
+    :param topic: the topic of the wikipedia page
+    """
+    return wiki.page(topic)
+
+
+
+
+
